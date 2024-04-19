f22e5283259a00b3331f3912af90f64e468ead72
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-19 17:34:00 +0900

    Added: change json to dartmodel map function

--- lib/features/posts/models/post_data_model.dart
+++ lib/features/posts/models/post_data_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class PostDataModel{
 
   final int userId;
@@ -11,4 +13,26 @@ class PostDataModel{
     required this.title, 
     required this.body});
   
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory PostDataModel.fromMap(Map<String, dynamic> map) {
+    return PostDataModel(
+      userId: map['userId']?.toInt() ?? 0,
+      id: map['id']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+      body: map['body'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory PostDataModel.fromJson(String source) => PostDataModel.fromMap(json.decode(source));
 }
+  
