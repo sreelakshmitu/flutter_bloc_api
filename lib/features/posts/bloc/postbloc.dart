d2eab12f0317b2316048b54c878deaff9637369e
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-19 15:31:45 +0900

    Added:  variable for holding response

--- lib/features/posts/bloc/postbloc.dart
+++ lib/features/posts/bloc/postbloc.dart
@@ -1,10 +1,33 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:http/http.dart' as http;
 
 part  'postevents.dart';
 part  'poststate.dart';
 
 class PostBloc extends Bloc<PostEvent,PostState>{
-  PostBloc():super(PostInitialState());
+  PostBloc():super(PostInitialState()){
+    on<PostInitialFetchEvent>(postinitialfetchevent);
+  }
+
+  FutureOr<void> postinitialfetchevent(PostInitialFetchEvent event, Emitter<PostState> emit) async{
+    
+     var client= http.Client();
+
+     try{
+      var response =await client.get(
+        Uri.https('https://jsonplaceholder.typicode.com/'),
+
+      );
+     }
+    catch(e){
+
+      log(e.toString());
+    }
+
+  }
 }
 
 
