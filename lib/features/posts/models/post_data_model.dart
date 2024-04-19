b3047af930fa747adc6da4d8b3705d98e166ef65
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-19 15:32:05 +0900

    Crerated: dart model for data

---
+++ lib/features/posts/models/post_data_model.dart
@@ -0,0 +1,14 @@
+class PostDataModel{
+
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  PostDataModel({
+    required this.userId, 
+    required this.id, 
+    required this.title, 
+    required this.body});
+  
+}
