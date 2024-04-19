5146741c7afc6d92c4065bf7dad6e300c1b0e580
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-19 17:34:15 +0900

    Added: initial state code

--- lib/features/posts/ui/postpage.dart
+++ lib/features/posts/ui/postpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api/features/posts/bloc/postbloc.dart';
 
 
 class PostPage extends StatefulWidget {
@@ -9,6 +10,12 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  @override
+  void initState() {
+    postbloc.add(PostInitialFetchEvent());
+    super.initState();
+  }
+  final PostBloc postbloc=PostBloc();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
