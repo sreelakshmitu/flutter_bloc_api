f46b096ff9b4b4ee4b13920e6992b6db342e29c6
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 15:49:15 +0900

    Created: function to post data

--- lib/features/posts/repos/post_repo.dart
+++ lib/features/posts/repos/post_repo.dart
@@ -28,4 +28,40 @@ class PostRepo{ //what it returns
       return[];
     }
   }
+
+  static Future<bool> writeposts(String id , String title , String body ,String userid) async{
+
+
+    var client=http.Client();
+
+    try{
+
+       var response = await client.post(Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+       body:{
+        "id":id,
+        "userId":userid,
+        "title":title,
+        "body":body,
+
+       });
+
+      // if posting becomes successful returns true , else false
+       
+       if(response.statusCode >=200 && response.statusCode < 300){
+        return true;
+       } else{
+        return false;
+       }
+
+        
+        }
+      catch(e){
+
+      log(e.toString());
+      return false;
+
+  }
+
+}
+
 }
