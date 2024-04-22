25df67d2d09dd65ab83f5aac3d9f728bf5e619f1
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 10:40:00 +0900

    Added : loading state

--- lib/features/posts/bloc/poststate.dart
+++ lib/features/posts/bloc/poststate.dart
@@ -2,6 +2,8 @@ part of 'postbloc.dart';
 
 abstract class PostState{}
 
+abstract class PostActionState extends PostState{}
+
 class PostInitialState extends PostState{}
 
 class PostInitialFetchState extends PostState{
@@ -9,4 +11,13 @@ class PostInitialFetchState extends PostState{
   final List<PostDataModel> dartresponse;
 
   PostInitialFetchState({required this.dartresponse});
+}
+
+class PostInitialLoadingState extends PostState{}
+
+class PostFetchSuccessfulState extends PostState{
+
+  final List<PostDataModel> post;
+
+  PostFetchSuccessfulState({required this.post});
 }
