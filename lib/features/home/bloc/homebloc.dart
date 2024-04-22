d514afc2ff0ad6a9da4748dc68b11325f63487b2
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 14:05:47 +0900

    Created:home bloc page

---
+++ lib/features/home/bloc/homebloc.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_api/features/home/bloc/homeevent.dart';
+import 'package:flutter_api/features/home/bloc/homestate.dart';
+
+class HomeBloc extends Bloc<HomeEvent,HomeState>{
+  HomeBloc():super(HomeInitialState()){
+
+    on<HomeInitialEvent>(homeinitialevent);
+    
+    on<HomeGetPostClickedEvent>(getpostclickedevent);
+    
+    on<HomeWritePostClickedEvent>(writepostclickedevent);
+
+    on<HomeUpdatePostClickedEvent>(updatepostclickedevent);
+
+    on<HomeDeletePostClickedEvent>(deletepostclickedevent);
+
+
+  }
+
+  FutureOr<void> homeinitialevent(HomeInitialEvent event, Emitter<HomeState> emit) {
+    
+    emit(HomeInitialState());
+  }
+
+  FutureOr<void> getpostclickedevent(HomeGetPostClickedEvent event, Emitter<HomeState> emit) {
+    
+    emit(HomeGetPostClickedState());
+  }
+
+  FutureOr<void> writepostclickedevent(event, Emitter<HomeState> emit) {
+    emit(HomeWritePostClickedState());
+  }
+
+  FutureOr<void> updatepostclickedevent(HomeUpdatePostClickedEvent event, Emitter<HomeState> emit) {
+    emit(HomeUpdatePostClickedState());
+  }
+
+  FutureOr<void> deletepostclickedevent(HomeDeletePostClickedEvent event, Emitter<HomeState> emit) {
+    emit(HomeDeletePostClickedState());
+  }
+
+
+}
