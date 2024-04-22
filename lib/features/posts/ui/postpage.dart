c3ea6fe870187dc11d7e159f5c745c871afb8be3
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 10:40:11 +0900

    Added : consumer bloc

--- lib/features/posts/ui/postpage.dart
+++ lib/features/posts/ui/postpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api/features/posts/bloc/postbloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 
 class PostPage extends StatefulWidget {
@@ -20,8 +22,50 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Posts')
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title:const Text('Posts Page')
       ),
+      body:BlocConsumer<PostBloc,PostState>(
+        bloc:postbloc,
+        listenWhen:(previous,current)=>current is PostActionState,
+        buildWhen:(previous,current)=> current is! PostActionState,
+        listener: (context,state){},
+        builder: (context,state){
+          switch(state.runtimeType){
+            case PostInitialLoadingState:
+            return const Center(
+              child:CircularProgressIndicator(),
+            );
+            case PostFetchSuccessfulState:
+            final fetchsucces=state as PostFetchSuccessfulState;
+            return  Scaffold(
+                body: ListView.builder(
+                  itemCount: fetchsucces.post.length,
+                  itemBuilder: (context,index){
+                    return Container(
+                      color:Colors.grey.shade200,
+                      padding: const EdgeInsets.all(16),
+                      margin:const EdgeInsets.all(16),
+                      child:Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(fetchsucces.post[index].title),
+                          Text(fetchsucces.post[index].body),
+
+                        ],
+                      )
+                    );
+                  }),
+            );
+           default:
+           return const SizedBox();
+
+          }
+
+        }, 
+        
+        )
     );
   }
 }
