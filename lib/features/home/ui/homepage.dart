c1f701f161645eba3ef3a2d7a2f73a4f95549c19
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 14:06:19 +0900

    Created : homepage ui

---
+++ lib/features/home/ui/homepage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api/features/home/bloc/homebloc.dart';
+import 'package:flutter_api/features/home/bloc/homeevent.dart';
+import 'package:flutter_api/features/home/bloc/homestate.dart';
+import 'package:flutter_api/features/posts/ui/postpage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatefulWidget {
+
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    homebloc.add(HomeInitialEvent());
+    super.initState();
+  }
+  final HomeBloc homebloc = HomeBloc();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:BlocConsumer<HomeBloc,HomeState>(
+        bloc:homebloc,
+        listenWhen:(previous,current)=>current is HomeState,
+        buildWhen:(previous,current)=>current is! HomeState,
+        listener:(context,state){
+          if(state is HomeGetPostClickedState){
+            Navigator.push(context, MaterialPageRoute(builder:(context)=> const PostPage()));
+          }
+          else if(state is HomeWritePostClickedState){}
+          else if(state is HomeUpdatePostClickedState){}
+          else if(state is HomeDeletePostClickedState){}
+        },
+        builder:(context,state){
+          return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+               style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue), 
+              ),
+              onPressed: (){homebloc.add(HomeGetPostClickedEvent());}, child: const Text('Get Posts',style:TextStyle(color: Colors.white))),
+              const SizedBox(height:20),
+              ElevatedButton(
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue), 
+              ),
+                onPressed: (){homebloc.add(HomeWritePostClickedEvent());}, child: const Text('Write Post',style:TextStyle(color: Colors.white))),
+              const SizedBox(height:20),
+              ElevatedButton(
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue), 
+              ),
+              onPressed: (){homebloc.add(HomeUpdatePostClickedEvent());}, child: const Text('Update Post',style:TextStyle(color: Colors.white))),
+              const SizedBox(height:20),
+              ElevatedButton(
+                style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue), 
+              ),
+                onPressed: (){homebloc.add(HomeDeletePostClickedEvent());}, child: const Text('Delete post',style:TextStyle(color: Colors.white))),
+            ],
+          ),
+        );
+        }
+      )
+     
+    );
+  }
+}
