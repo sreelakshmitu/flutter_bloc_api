d0ef73ebbe39b475af9c85e1f397e4b942bfe0e8
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 14:06:00 +0900

    Created:home events

---
+++ lib/features/home/bloc/homeevent.dart
@@ -0,0 +1,11 @@
+abstract class HomeEvent{}
+
+class HomeInitialEvent extends HomeEvent{}
+
+class HomeGetPostClickedEvent extends HomeEvent{}
+
+class HomeWritePostClickedEvent extends HomeEvent{}
+
+class HomeUpdatePostClickedEvent extends HomeEvent{}
+
+class HomeDeletePostClickedEvent extends HomeEvent{}
