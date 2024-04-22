97a64b3feb90d05efdc6cb5ac7d35e2af358062d
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 15:50:17 +0900

    Created : write page

---
+++ lib/features/write/ui/writepage.dart
@@ -0,0 +1,114 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_api/features/write/bloc/writebloc.dart';
+import 'package:flutter_api/features/write/bloc/writeevent.dart';
+import 'package:flutter_api/features/write/bloc/writestate.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+
+class WritePage extends StatefulWidget {
+  const WritePage({super.key});
+
+  @override
+  State<WritePage> createState() => _WritePageState();
+}
+
+class _WritePageState extends State<WritePage> {
+  @override
+  void initState() {
+    writebloc.add(WriteInitialEvent());
+    super.initState();
+  }
+  final WriteBloc writebloc=WriteBloc();
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController useridcontroller=TextEditingController();
+    final TextEditingController idcontroller=TextEditingController();
+    final TextEditingController titlecontroller=TextEditingController();
+    final TextEditingController bodycontroller=TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+      ),
+      body:BlocConsumer<WriteBloc,WriteState>(
+        bloc:writebloc,
+        listener:(context,state){},
+        builder: (context,state) { 
+            return Center(
+            child: Container(
+            padding:const EdgeInsets.only(left:10,right:10,bottom:10),
+            width:double.maxFinite,
+            height:400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: useridcontroller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter userid',
+                    border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),),
+                ),
+                const SizedBox(height:10),
+                TextField(
+                  controller: idcontroller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter id',
+                    border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),),
+                ),
+                const SizedBox(height:10),
+                TextField(
+                  controller: titlecontroller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter title',
+                    border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),),
+                ),
+                const SizedBox(height:10),
+                TextField(
+                  maxLines: null,
+                  controller: bodycontroller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter body',
+                    border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),),
+                ),
+                const SizedBox(height:10),
+                ElevatedButton(
+                onPressed: () {
+                writebloc.add(AddDataClickedEvent(
+                id: idcontroller.value.text,
+                userid: useridcontroller.value.text,
+                title: titlecontroller.value.text,
+                body: bodycontroller.value.text,
+                ));
+    
+   
+               idcontroller.clear();
+               useridcontroller.clear();
+               titlecontroller.clear();
+               bodycontroller.clear();
+               },
+              style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              ),
+              child: const Text('Add Data', style: TextStyle(color: Colors.white)),
+              ),
+
+              ],
+            ),
+          ),
+        );
+        }
+      )
+    );
+  }
+}
