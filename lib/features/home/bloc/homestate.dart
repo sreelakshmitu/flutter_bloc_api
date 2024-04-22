58fae2b217798f66a27d8dd5e171e52d0237dfea
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-22 14:06:08 +0900

    Created: home states

---
+++ lib/features/home/bloc/homestate.dart
@@ -0,0 +1,13 @@
+abstract class HomeState{}
+
+abstract class HomeActionState extends HomeState{}
+
+class HomeInitialState extends HomeState{}
+
+class HomeGetPostClickedState extends HomeActionState{}
+
+class HomeWritePostClickedState extends HomeActionState{}
+
+class HomeUpdatePostClickedState extends HomeActionState{}
+
+class HomeDeletePostClickedState extends HomeActionState{}
