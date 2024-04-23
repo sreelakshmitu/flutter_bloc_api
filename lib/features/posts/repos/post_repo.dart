ef156d9ffcb700de1a34df8f143cef754f8e8447
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-23 10:04:36 +0900

    Added : function for updating post

--- lib/features/posts/repos/post_repo.dart
+++ lib/features/posts/repos/post_repo.dart
@@ -64,4 +64,39 @@ class PostRepo{ //what it returns
 
 }
 
+static Future<bool> updateposts(String id , String title , String body ,String userid) async{
+
+
+    var client=http.Client();
+
+    try{
+
+       var response = await client.put(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'),
+       body:{
+        "id":id,
+        "userId":userid,
+        "title":title,
+        "body":body,
+
+       });
+
+      // if posting becomes successful returns true , else false
+       
+       if(response.statusCode >=200 && response.statusCode < 300){
+        return true;
+       } else{
+        return false;
+       }
+
+        
+        }
+      catch(e){
+
+      log(e.toString());
+      return false;
+
+  }
+
+}
+
 }
