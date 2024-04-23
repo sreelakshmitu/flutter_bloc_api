e48d1496154aa7c6caa9cb1aa66b5517fc2cbe69
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-23 11:09:47 +0900

    Added : style for postpage title

--- lib/features/posts/ui/postpage.dart
+++ lib/features/posts/ui/postpage.dart
@@ -24,7 +24,7 @@ class _PostPageState extends State<PostPage> {
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
-        title:const Text('Posts Page')
+        title:const Text('Posts Page',style: TextStyle(color:Colors.white),)
       ),
       body:BlocConsumer<PostBloc,PostState>(
         bloc:postbloc,
