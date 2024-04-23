872cd074419936be6e82b7a0769824a6eb249c6a
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-23 11:08:28 +0900

    Created: delete bloc file

---
+++ lib/features/delete/bloc/deletebloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter_api/features/delete/bloc/deleteevent.dart';
+import 'package:flutter_api/features/delete/bloc/deletestate.dart';
+import 'package:flutter_api/features/posts/repos/post_repo.dart';
+
+class DeleteBloc extends Bloc<DeleteEvent,DeleteState>{
+  DeleteBloc():super(DeleteInitialState()){
+    on<DeleteInitialEvent>(deleteinitialevent);
+    on<DeleteButtonClickedEvent>(deletebuttonclickedevent);
+  }
+
+   FutureOr<void> deleteinitialevent(DeleteInitialEvent event, Emitter<DeleteState> emit) {
+
+    emit(DeleteInitialState());
+  }
+  
+
+  FutureOr<void> deletebuttonclickedevent(DeleteButtonClickedEvent event, Emitter<DeleteState> emit) async{
+
+    bool success=await PostRepo.deleteposts(event.id);
+
+    //print(success)
+
+    if(success){
+      emit(DeleteDataSuccessfulState());}
+
+    else{
+      emit(DeleteDataErrorState());
+    }
+
+
+
+  }
+
+ 
+}
