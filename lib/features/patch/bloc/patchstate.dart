c19b902a86063cabd2f896cb9847fe97931ca1e8
Sreelakshmi T U <tusreelakshmi1@gmail.com>
2024-04-24 14:43:18 +0900

    Created : states for patch

---
+++ lib/features/patch/bloc/patchstate.dart
@@ -0,0 +1,14 @@
+abstract class PatchState{}
+
+
+
+class PatchInitialState extends PatchState{}
+
+class PatchDataSelectedState extends PatchState{}
+
+class PatchDataAddedSuccesfulState extends PatchState{}
+
+class PatchDataErrorState extends PatchState{}
+
+
+
